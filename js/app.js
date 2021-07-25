5753a9a5a6e0de0835976f594c4f32d2cef0fafa
Andrew Dubovik <40122908+dubofweek@users.noreply.github.com>
2021-07-25 16:36:07 +0300

    update

---
+++ js/app.js
@@ -0,0 +1,91 @@
+import { Swiper, Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation } from 'swiper'
+Swiper.use ([Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation])
+
+import { gsap, Power2 } from 'gsap'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const swiperIMG = new Swiper('.slider-img', {
+		loop: false,
+		speed: 2400,
+		parallax: true,
+		pagination: {
+			el: '.slider-pagination-count .total',
+			type: 'custom',
+			renderCustom: function(swiper,current,total) {
+				return `0${total}`
+			}
+		}
+	})
+
+	const swiperText = new Swiper('.slider-text', {
+		loop: false,
+		speed: 2400,
+		parallax: true,
+		mousewheel: {
+			invert: false
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true
+		},
+		navigation: {
+			prevEl:'.swiper-button-prev',
+			nextEl:'.swiper-button-next'
+		}
+	})
+
+	swiperIMG.controller.control = swiperText
+	swiperText.controller.control = swiperIMG
+
+	let gear = document.querySelector('.slider-gear')
+
+	swiperText.on('slideNextTransitionStart', function() {
+		gsap.to(gear, 2.8, {
+			rotation: '+=40',
+			ease: Power2.easeOut
+		})
+	})
+	swiperText.on('slidePrevTransitionStart', function() {
+		gsap.to(gear, 2.8, {
+			rotation: '-=40',
+			ease: Power2.easeOut
+		})
+	})
+
+	let curnum = document.querySelector('.slider-pagination-count .current'),
+		pagcur = document.querySelector('.slider-pagination-current__num')
+
+	
+	swiperText.on('slideChange', function() {
+		let ind = swiperText.realIndex + 1
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			ease: Power2.easeOut,
+			onComplete: function() {
+				gsap.to(curnum, .1, {
+					force3D: true ,
+					y: 10
+				})
+				curnum.innerHTML = `0${ind}`
+				pagcur.innerHTML = `0${ind}`
+			}
+		})
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: 0,
+	 		opacity: 1,
+			ease: Power2.easeOut,
+			delay: .3
+		})
+	})
+
+})
+
+
